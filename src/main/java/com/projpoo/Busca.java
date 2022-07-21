d33c49b2a6e2489cc87e177a3a1cdc4c0608408e
AdaltonF <adaltonfilho2003@gmail.com>
2022-07-21 10:41:15 -0300

    Busca

--- src/main/java/com/projpoo/Busca.java
+++ src/main/java/com/projpoo/Busca.java
@@ -2,16 +2,32 @@ package com.projpoo;
 
 import info.movito.themoviedbapi.TmdbApi;
 import info.movito.themoviedbapi.TmdbSearch;
+import info.movito.themoviedbapi.model.MovieDb;
 import info.movito.themoviedbapi.model.core.MovieResultsPage;
 
 public class Busca {
 
-    public static void main(String[] args) {
-        TmdbSearch buscador = new TmdbSearch(new TmdbApi("2b101da85c41bf5cb1ce101ebb69b33c"));
-        MovieResultsPage searchMovies = new MovieResultsPage();
-        searchMovies = buscador.searchMovie("Thor", 0, "pt-br", false, 0);
+    static String apiKey = "2b101da85c41bf5cb1ce101ebb69b33c";
 
-        MovieResultsPage popMovies = new MovieResultsPage();
+    public static void filme(String nomeFilme) {
+        TmdbSearch buscador = new TmdbSearch(new TmdbApi(apiKey));
+        MovieResultsPage searchMovies = new MovieResultsPage();
+        try {
+            searchMovies = buscador.searchMovie(nomeFilme, 0, "pt-br", false, 0);    
+        } catch (RuntimeException e) {
+            System.out.println("Campo de busca vazio!");
+            return;
+        }
+        
+
+        for (MovieDb movieDb : searchMovies.getResults()) {
+            System.out.println(movieDb.getTitle());
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        
     }
 
 }
