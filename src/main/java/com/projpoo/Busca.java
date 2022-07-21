e3c1d893ecc9fa953358d15ca5bfdd6f0babeb30
AdaltonF <adaltonfilho2003@gmail.com>
2022-07-21 11:49:43 -0300

    Finalizando busca

--- src/main/java/com/projpoo/Busca.java
+++ src/main/java/com/projpoo/Busca.java
@@ -1,5 +1,7 @@
 package com.projpoo;
 
+import java.util.List;
+
 import info.movito.themoviedbapi.TmdbApi;
 import info.movito.themoviedbapi.TmdbSearch;
 import info.movito.themoviedbapi.model.MovieDb;
@@ -9,24 +11,30 @@ public class Busca {
 
     static String apiKey = "2b101da85c41bf5cb1ce101ebb69b33c";
 
-    public static void filme(String nomeFilme) {
+    public static List<MovieDb> filme(String nomeFilme) {
         TmdbSearch buscador = new TmdbSearch(new TmdbApi(apiKey));
         MovieResultsPage searchMovies = new MovieResultsPage();
         try {
             searchMovies = buscador.searchMovie(nomeFilme, 0, "pt-br", false, 0);    
+            return searchMovies.getResults();
         } catch (RuntimeException e) {
-            System.out.println("Campo de busca vazio!");
-            return;
-        }
-        
-
-        for (MovieDb movieDb : searchMovies.getResults()) {
-            System.out.println(movieDb.getTitle());
+            //System.out.println("Campo de busca vazio!");
+            return searchMovies.getResults(); 
         }
     }
 
     public static void main(String[] args) {
+        List<MovieDb> rsultado = Busca.filme("");
         
+        int cont = 5;
+        if (rsultado != null) {
+            for (MovieDb movieDb : rsultado) {
+                System.out.println(movieDb.getTitle());
+                cont--;
+                if (cont == 0) break;
+            }
+        } else
+            System.out.println("A busca não encoutrou filmes!");
         
     }
 
