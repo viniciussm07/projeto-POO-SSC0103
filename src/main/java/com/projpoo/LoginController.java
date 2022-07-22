93013c8086e25bb0f3fb815037a93086afdff7bc
vinicius <vini.soares.martins@gmail.com>
2022-07-22 11:11:06 -0300

    setando login

---
+++ src/main/java/com/projpoo/LoginController.java
@@ -0,0 +1,37 @@
+package com.projpoo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.MouseEvent;
+
+public class LoginController {
+
+    @FXML
+    private Button botaoLogin;
+
+    @FXML
+    private TextField campoSenha;
+
+    @FXML
+    private TextField campoUsuario;
+
+    @FXML
+    void fazerLogin(ActionEvent event) {
+        String usuario = campoUsuario.getText();
+        String senha = campoSenha.getText();
+        if (usuario.equals("usuario") && senha.equals("usuario")) {
+            System.out.println("Logado com sucesso!");
+        } else {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setHeaderText("Erro");
+            alert.setTitle("Login Inválido");
+            alert.setContentText("Usuário ou senha inválido");
+            alert.show();
+        }
+    }
+
+}
--- src/main/java/com/projpoo/Main.java
+++ src/main/java/com/projpoo/Main.java
@@ -6,14 +6,20 @@ import javafx.scene.Scene;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
 
-public class TestandoJavaFx extends Application{
+public class Main extends Application{
+
+    Stage window;
+    Scene scene1, scene2;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("layout.fxml"));
+        window = primaryStage;
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("loginLayout.fxml"));
         Parent root = fxmlLoader.load();
         Scene tela = new Scene(root);
 
