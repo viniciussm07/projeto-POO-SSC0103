4d0deca78e5c2a7da35027a9c3dad416607dee0b
vinicius <vini.soares.martins@gmail.com>
2022-07-22 22:48:45 -0300

    seting images

--- src/main/java/com/projpoo/controllers/HomeController.java
+++ src/main/java/com/projpoo/controllers/HomeController.java
@@ -2,6 +2,9 @@ package com.projpoo.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 
@@ -9,6 +12,7 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -17,38 +21,71 @@ import javafx.scene.Node;
 public class HomeController {
 
     @FXML
+    private AnchorPane anchorRoot;
+
+    @FXML
     private Button botaoHome;
 
     @FXML
     private Button botaoMinhaLista;
 
     @FXML
-    private HBox hboxFilms;
+    private Button carregaFilmes;
 
-    @FXML
-    private Button plus;
-
-    public void switchToMinhaLista(ActionEvent event) throws IOException{
+    public void switchToMinhaLista(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("../views/minhaListaLayout.fxml"));
-        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
     @FXML
-    void add(ActionEvent event) {
-        Pane pane1 = new Pane();
-        pane1.setStyle("-fx-background-color: #000000;");
-        pane1.setPrefHeight(164.0);
-        pane1.setPrefWidth(121.0);
-
-        Pane pane2 = new Pane();
-        pane2.setStyle("-fx-background-color: #ffffff;");
-        hboxFilms.getChildren().addAll(pane1, pane2);
-        // prefHeight="164.0" prefWidth="121.0" style="-fx-background-color: #D9D9D9;
-        System.out.println(hboxFilms.getChildren());
+    void adding(ActionEvent event) {
+        HBox hbox = addHbox();
+        Pane pane = createPane();
+        hbox.getChildren().addAll(pane);
+        anchorRoot.getChildren().addAll(hbox);
     }
 
+    // @FXML
+    // ImageView addFilmImage() {
+    //     ImageView filmImage = new ImageView();
+    //     filmImage.setFitHeight(164.0);
+    //     filmImage.setFitWidth(121.0);
+    //     filmImage.setPickOnBounds(true);
+    //     filmImage.setPreserveRatio(true);
+    //     // altrerar a linha de baixo e adicionar a função para puxar a imagem da api
+    //     Image img = new Image(
+    //             "https://disney.com.br/novidades/quando-doutor-estranho-no-multiverso-da-loucura-estreia-no-disney-plus");
+
+    //     filmImage.setImage(img);
+
+    //     System.out.println("testando");
+
+    //     return filmImage;
+    // }
+
+    @FXML
+    Pane createPane() {
+        Pane pane1 = new Pane();
+        pane1.setStyle("-fx-background-image: url('https://lumiere-a.akamaihd.net/v1/images/beta_epic_payoff_brazil_2a5f625f.jpeg');-fx-background-repeat: no-repeat;-fx-background-size: 174 166;-fx-background-position: center center;");
+        HBox.setMargin(pane1, new Insets(0, 25, 0, 25));
+        pane1.setPrefWidth(135.0);
+
+        return pane1;
+    }
+
+    @FXML
+    HBox addHbox() {
+        HBox hbox = new HBox();
+        hbox.setLayoutY(94.0);
+        hbox.setPrefSize(640.0, 166.0);
+        AnchorPane.setRightAnchor(hbox, 0.0);
+        AnchorPane.setLeftAnchor(hbox, 0.0);
+
+        return hbox;
+    }
 
 }
